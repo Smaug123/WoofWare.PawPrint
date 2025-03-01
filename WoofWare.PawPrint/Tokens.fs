f008c3166cb406becbd78ac5f9755531117d6fe1
Smaug123 <3138005+Smaug123@users.noreply.github.com>
2025-03-01 15:22:06 +0000

    More types

--- WoofWare.PawPrint/Tokens.fs
+++ WoofWare.PawPrint/Tokens.fs
@@ -9,7 +9,6 @@ type MetadataToken =
     | MethodSpecification of MethodSpecificationHandle
     | MemberReference of MemberReferenceHandle
     | TypeReference of TypeReferenceHandle
-    | ModuleDefinition of ModuleDefinitionHandle
     | AssemblyReference of AssemblyReferenceHandle
     | TypeSpecification of TypeSpecificationHandle
     | TypeDefinition of TypeDefinitionHandle
@@ -19,6 +18,22 @@ type MetadataToken =
     | ExportedType of ExportedTypeHandle
     | StandaloneSignature of StandaloneSignatureHandle
     | EventDefinition of EventDefinitionHandle
+    | Constant of ConstantHandle
+    | CustomAttribute of CustomAttributeHandle
+    | DeclarativeSecurityAttribute of DeclarativeSecurityAttributeHandle
+    | PropertyDefinition of PropertyDefinitionHandle
+    | ModuleReference of ModuleReferenceHandle
+    | AssemblyFile of AssemblyFileHandle
+    | ManifestResource of ManifestResourceHandle
+    | GenericParameter of GenericParameterHandle
+    | GenericParameterConstraint of GenericParameterConstraintHandle
+    | Document of DocumentHandle
+    | MethodDebugInformation of MethodDebugInformationHandle
+    | LocalScope of LocalScopeHandle
+    | LocalVariable of LocalVariableHandle
+    | LocalConstant of LocalConstantHandle
+    | ImportScope of ImportScopeHandle
+    | CustomDebugInformation of CustomDebugInformationHandle
 
 [<RequireQualifiedAccess>]
 module MetadataToken =
@@ -26,7 +41,7 @@ module MetadataToken =
         let asRowNum = value &&& 0x00FFFFFF
 
         match LanguagePrimitives.EnumOfValue<byte, HandleKind> (byte (value &&& 0xFF000000 >>> 24)) with
-        | HandleKind.ModuleDefinition -> MetadataToken.ModuleDefinition (failwith "TODO")
+        | HandleKind.ModuleDefinition -> failwith "TODO"
         | HandleKind.TypeReference -> MetadataToken.TypeReference (MetadataTokens.TypeReferenceHandle asRowNum)
         | HandleKind.TypeDefinition -> MetadataToken.TypeDefinition (MetadataTokens.TypeDefinitionHandle asRowNum)
         | HandleKind.FieldDefinition -> MetadataToken.FieldDefinition (MetadataTokens.FieldDefinitionHandle asRowNum)
@@ -35,40 +50,44 @@ module MetadataToken =
         | HandleKind.InterfaceImplementation ->
             MetadataToken.InterfaceImplementation (MetadataTokens.InterfaceImplementationHandle asRowNum)
         | HandleKind.MemberReference -> MetadataToken.MemberReference (MetadataTokens.MemberReferenceHandle asRowNum)
-        | HandleKind.Constant -> failwith "todo"
-        | HandleKind.CustomAttribute -> failwith "todo"
-        | HandleKind.DeclarativeSecurityAttribute -> failwith "todo"
+        | HandleKind.Constant -> MetadataToken.Constant (MetadataTokens.ConstantHandle asRowNum)
+        | HandleKind.CustomAttribute -> MetadataToken.CustomAttribute (MetadataTokens.CustomAttributeHandle asRowNum)
+        | HandleKind.DeclarativeSecurityAttribute ->
+            MetadataToken.DeclarativeSecurityAttribute (MetadataTokens.DeclarativeSecurityAttributeHandle asRowNum)
         | HandleKind.StandaloneSignature ->
             MetadataToken.StandaloneSignature (MetadataTokens.StandaloneSignatureHandle asRowNum)
         | HandleKind.EventDefinition -> MetadataToken.EventDefinition (MetadataTokens.EventDefinitionHandle asRowNum)
-        | HandleKind.PropertyDefinition -> failwith "todo"
+        | HandleKind.PropertyDefinition -> MetadataToken.PropertyDefinition (MetadataTokens.PropertyDefinitionHandle asRowNum)
         | HandleKind.MethodImplementation ->
             MetadataToken.MethodImplementation (MetadataTokens.MethodImplementationHandle asRowNum)
-        | HandleKind.ModuleReference -> failwith "todo"
+        | HandleKind.ModuleReference -> MetadataToken.ModuleReference (MetadataTokens.ModuleReferenceHandle asRowNum)
         | HandleKind.TypeSpecification ->
             MetadataToken.TypeSpecification (MetadataTokens.TypeSpecificationHandle asRowNum)
-        | HandleKind.AssemblyDefinition -> failwith "todo"
+        | HandleKind.AssemblyDefinition -> failwith "TODO"
         | HandleKind.AssemblyReference ->
             MetadataToken.AssemblyReference (MetadataTokens.AssemblyReferenceHandle asRowNum)
-        | HandleKind.AssemblyFile -> failwith "todo"
+        | HandleKind.AssemblyFile -> MetadataToken.AssemblyFile (MetadataTokens.AssemblyFileHandle asRowNum)
         | HandleKind.ExportedType -> MetadataToken.ExportedType (MetadataTokens.ExportedTypeHandle asRowNum)
-        | HandleKind.ManifestResource -> failwith "todo"
-        | HandleKind.GenericParameter -> failwith "todo"
+        | HandleKind.ManifestResource -> MetadataToken.ManifestResource (MetadataTokens.ManifestResourceHandle asRowNum)
+        | HandleKind.GenericParameter -> MetadataToken.GenericParameter (MetadataTokens.GenericParameterHandle asRowNum)
         | HandleKind.MethodSpecification ->
             MetadataToken.MethodSpecification (MetadataTokens.MethodSpecificationHandle asRowNum)
-        | HandleKind.GenericParameterConstraint -> failwith "todo"
-        | HandleKind.Document -> failwith "todo"
-        | HandleKind.MethodDebugInformation -> failwith "todo"
-        | HandleKind.LocalScope -> failwith "todo"
-        | HandleKind.LocalVariable -> failwith "todo"
-        | HandleKind.LocalConstant -> failwith "todo"
-        | HandleKind.ImportScope -> failwith "todo"
-        | HandleKind.CustomDebugInformation -> failwith "todo"
-        | HandleKind.UserString -> failwith "todo"
-        | HandleKind.Blob -> failwith "todo"
-        | HandleKind.Guid -> failwith "todo"
-        | HandleKind.String -> failwith "todo"
-        | HandleKind.NamespaceDefinition -> failwith "todo"
+        | HandleKind.GenericParameterConstraint ->
+            MetadataToken.GenericParameterConstraint (MetadataTokens.GenericParameterConstraintHandle asRowNum)
+        | HandleKind.Document -> MetadataToken.Document (MetadataTokens.DocumentHandle asRowNum)
+        | HandleKind.MethodDebugInformation ->
+            MetadataToken.MethodDebugInformation (MetadataTokens.MethodDebugInformationHandle asRowNum)
+        | HandleKind.LocalScope -> MetadataToken.LocalScope (MetadataTokens.LocalScopeHandle asRowNum)
+        | HandleKind.LocalVariable -> MetadataToken.LocalVariable (MetadataTokens.LocalVariableHandle asRowNum)
+        | HandleKind.LocalConstant -> MetadataToken.LocalConstant (MetadataTokens.LocalConstantHandle asRowNum)
+        | HandleKind.ImportScope -> MetadataToken.ImportScope (MetadataTokens.ImportScopeHandle asRowNum)
+        | HandleKind.CustomDebugInformation ->
+            MetadataToken.CustomDebugInformation (MetadataTokens.CustomDebugInformationHandle asRowNum)
+        | HandleKind.UserString -> failwith "TODO"
+        | HandleKind.Blob -> failwith "TODO"
+        | HandleKind.Guid -> failwith "TODO"
+        | HandleKind.String -> failwith "TODO"
+        | HandleKind.NamespaceDefinition -> failwith "TODO"
         | h -> failwith $"Unrecognised kind: {h}"
 
     let ofEntityHandle (eh : EntityHandle) : MetadataToken = ofInt (eh.GetHashCode ())
