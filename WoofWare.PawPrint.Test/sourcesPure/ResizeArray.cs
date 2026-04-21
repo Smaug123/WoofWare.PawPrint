b08c5cc748d57cc35525d55904a59d693c8d51ac
Patrick Stevens <3138005+Smaug123@users.noreply.github.com>
2026-04-21 08:45:51 +0100

    Annotate the tests to say why they fail (#213)

--- WoofWare.PawPrint.Test/sourcesPure/ResizeArray.cs
+++ WoofWare.PawPrint.Test/sourcesPure/ResizeArray.cs
@@ -12,7 +12,17 @@ namespace HelloWorldApp
             l.Add(100);
             var m = l.Select(x => x.ToString()).ToList();
             // 2 + 108 + (1 + 3) = 114
-            return m.Count + l.Sum() + m.Select(x => x.Length).Sum();
+            var result = m.Count + l.Sum() + m.Select(x => x.Length).Sum();
+            if (result == 0)
+            {
+                return 1;
+            }
+            if (result != 114)
+            {
+                return result;
+            }
+
+            return 0;
         }
     }
 }
