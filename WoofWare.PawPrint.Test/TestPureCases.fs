8081a43b8c35cf6f0b1f4d0de018815da9421008
Patrick Stevens <3138005+Smaug123@users.noreply.github.com>
2026-04-25 07:39:18 +0100

    Treat ptr[void] in initobj (#258)

--- WoofWare.PawPrint.Test/TestPureCases.fs
+++ WoofWare.PawPrint.Test/TestPureCases.fs
@@ -17,7 +17,7 @@ module TestPureCases =
 
     let unimplemented =
         [
-            "CrossAssemblyTypes.cs" // "BUG: reached extern dispatch for IRuntimeFieldInfo::get_Value"
+            "CrossAssemblyTypes.cs" // initobj ptr[void] now works; blocked downstream on RuntimeFieldHandle.GetRVAFieldInfo P/Invoke boundary
             "EnumSemantics.cs" // Constrained works; blocked on System.Object.GetType JIT intrinsic reached from Object.ToString
             "OverlappingStructs.cs" // blocked on Marshal.SizeOfHelper PInvoke boundary
             "AdvancedStructLayout.cs" // "TODO: couldn't identify field at offset"
